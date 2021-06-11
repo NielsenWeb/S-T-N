5f38832f1bf11e48f47e756947148f21171909aa
Nowak92 <85220895+Nowak92@users.noreply.github.com>
2021-06-11 11:36:47 +0200

    doantion
    modal pop up for donations added

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-var dropMenu = document.getElementById('nav-mobile-drop');
-var closeLayer = document.getElementById('close-layer');
 
 
+var dropMenu = document.getElementById("nav-mobile-drop");
+var closeLayer = document.getElementById("close-layer");
+var modalDonation = document.getElementById('modal-donation')
 
-function dropDown(){
-    
-    
 
-    if(dropMenu.style.width == '250px'){
-        dropMenu.style.width = '0';
-        closeLayer.style.display = 'block';
-    }
-
-    else{
-        closeLayer.style.display = 'none';
-        dropMenu.style.width = '250px';
-    }
-    
+function dropDown() {
+  if (dropMenu.style.width == "150px") {
+    dropMenu.style.width = "0";
+    closeLayer.style.display = "block";
+  } else {
+    closeLayer.style.display = "none";
+    dropMenu.style.width = "150px";
+  }
 }
 
-function closeDropDown(){
-    closeLayer.style.display = 'none'
-    dropMenu.style.width = '0'
+function closeDropDown() {
+  closeLayer.style.display = "none";
+  dropMenu.style.width = "0";
+}
+
+function openModalDonation() {
+    modalDonation.style.display = 'block'
+}
+
+function closeModalDonation(){
+    modalDonation.style.display = 'none'
 }
