fb2d5e75b88a387f89084006822eba04f0d89dd6
Nowak92 <85220895+Nowak92@users.noreply.github.com>
2021-06-14 18:42:06 +0200

    imageslider added

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 
 var dropMenu = document.getElementById("nav-mobile-drop");
 var closeLayer = document.getElementById("close-layer");
+
 var modalDonation = document.getElementById('modal-donation')
 var modalDonationScreen = document.getElementById('modal-donation-screen')
 
+
+
+/* mobile Dropdown */
 
 function dropDown() {
   if (dropMenu.style.width == "150px") {
@@ -22,7 +26,7 @@ function closeDropDown() {
 }
 
 
-
+/* modal for donations */
 function openModalDonation() {
     modalDonation.style.display = 'block'
 }
@@ -39,4 +43,19 @@ function closeModalDonationScreen(){
   modalDonationScreen.style.display = 'none'
 }
 
+/* Slideshow */
 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000);
+}
