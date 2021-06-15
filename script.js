375996f6581279a6c5a14bf3c52872cd24e3c509
Nowak92 <85220895+Nowak92@users.noreply.github.com>
2021-06-15 18:07:26 +0200

    Form and validation added

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-
-
+/* DropDown  */
 var dropMenu = document.getElementById("nav-mobile-drop");
 var closeLayer = document.getElementById("close-layer");
 
-var modalDonation = document.getElementById('modal-donation')
-var modalDonationScreen = document.getElementById('modal-donation-screen')
+/* Donation modal */
+var modalDonation = document.getElementById("modal-donation");
+var modalDonationScreen = document.getElementById("modal-donation-screen");
 
+var slideIndex = 0;
 
+/*contact validate */
+var mailformat =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var submitButton = document.getElementById("form-submit");
+var emailInput = document.getElementById("email-input");
+var fnameInput = document.getElementById("fname-input");
+var lnameInput = document.getElementById("lname-input");
+
+showSlides();
 
 /* mobile Dropdown */
 
@@ -25,28 +35,24 @@ function closeDropDown() {
   dropMenu.style.width = "0";
 }
 
-
 /* modal for donations */
 function openModalDonation() {
-    modalDonation.style.display = 'block'
+  modalDonation.style.display = "block";
 }
 
-function closeModalDonation(){
-    modalDonation.style.display = 'none'
+function closeModalDonation() {
+  modalDonation.style.display = "none";
 }
 
-function openModalDonationScreen(){
-  modalDonationScreen.style.display = 'block'
+function openModalDonationScreen() {
+  modalDonationScreen.style.display = "block";
 }
 
-function closeModalDonationScreen(){
-  modalDonationScreen.style.display = 'none'
+function closeModalDonationScreen() {
+  modalDonationScreen.style.display = "none";
 }
 
 /* Slideshow */
-
-var slideIndex = 0;
-showSlides();
 
 function showSlides() {
   var i;
@@ -55,17 +61,55 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 10000);
 }
 
-fadeOut();
+/* contact form validation */
 
-function fadeOut() {
-  if (document.getElementById('fading').style.opacity == '1') {
+submitButton.addEventListener("click", function (event) {});
 
-  console.log('test')
+function validateLname() {
+  if (document.contact.lname.value == "") {
+    lnameInput.style.borderStyle = "solid";
+    lnameInput.style.borderColor = "red";
+    document.contact.lname.focus();
+    lnameInput.style.backgroundColor = "lightsalmon";
+    event.preventDefault();
+    return false;
+  } else {
+    lnameInput.style.borderStyle = "none";
+    lnameInput.style.backgroundColor = "#354463";
   }
+}
 
+function validateFname() {
+  if (document.contact.fname.value == "") {
+    fnameInput.style.borderStyle = "solid";
+    fnameInput.style.borderColor = "red";
+    document.contact.fname.focus();
+    event.preventDefault();
+    fnameInput.style.backgroundColor = "lightsalmon";
+    return false;
+  } else {
+    fnameInput.style.borderStyle = "none";
+    fnameInput.style.backgroundColor = "#354463";
+  }
+}
+
+function ValidateEmail(inputText) {
+  if (inputText.value.match(mailformat)) {
+    return true;
+  } else {
+    event.preventDefault();
+    document.contact.email.focus();
+    emailInput.style.borderStyle = "solid";
+    emailInput.style.borderColor = "red";
+    emailInput.style.backgroundColor = "lightsalmon";
+
+    return false;
+  }
 }
